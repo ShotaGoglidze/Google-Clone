02278f2d0bb01e61a6cad10fe25587ea0f9b0d86
Shota Goglidze <sg@gmail.com>
2024-02-25 19:59:16 -0800

    add body section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
        <HomeHeader />
+       <div className="flex flex-col items-center mt-24">
+         <Image
+           src="https://freelogopng.com/images/all_img/1657952217google-logo-png.png"
+           alt="Google Logo"
+           width={300}
+           height={100}
+           priority
+           style={{width: 'auto'}}
+         />
+        <HomeSearch />
+       </div>
     </>
   )
 }
